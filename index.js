24cc20c8d44e4df49133d1ac9f1736b3aef5617c
Omolara5861 <debbiegterra@gmail.com>
2022-05-21 11:25:58 +0100

    created a folder to log the messages

--- index.js
+++ index.js
@@ -4,17 +4,25 @@
 // })
 
 const Logger = require("./logger");
+const fs = require('fs');
+const path = require('path');
 
 //Init Object
 const msgLogger = new Logger();
 
 //Event Lister 
-msgLogger.on('message', data => console.log('Message Looged:', data));
+msgLogger.on('message', data => {
+    //Created a folder to store the Logged messages
+    fs.mkdir(path.join(__dirname, 'messages'), {}, err => {
+        if(err) throw err;
+        console.log('Folder Created!');
+    })
+})
+
 
 //Call the Event
 msgLogger.log('Good Morning!');
 msgLogger.log('How was your Night?');
 msgLogger.log('Was great yours?');
 msgLogger.log('Awesome, thank you.');
-
 
