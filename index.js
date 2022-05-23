e28cfed09be8d7edbdf271afedf1ddaea96230e0
Omolara5861 <debbiegterra@gmail.com>
2022-05-23 13:14:28 +0100

    rendered home in the brower

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const path = require('path');
 const PORT = process.env.PORT || 1908;
 
 const server = http.createServer((req , res) => {
-
+    if(req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
