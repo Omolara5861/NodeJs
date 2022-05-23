82e80ef6fa58977f7efea8de9e47c4f5a7baa908
Omolara5861 <debbiegterra@gmail.com>
2022-05-23 12:56:49 +0100

    brought in the required module

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //Module Function Wrapper
 // (function(module, require, exports, __dirname, __filename) {
 // })
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
