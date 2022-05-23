189eaa601002e35c655ee9779429320d9b1dc2fb
Omolara5861 <debbiegterra@gmail.com>
2022-05-23 13:19:04 +0100

    rendered about page

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const server = http.createServer((req , res) => {
             res.end(content);
         })
     }
+    else if(req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
