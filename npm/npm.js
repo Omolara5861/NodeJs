7ab830d10b21bcf3e3ac81ee9cd2ec3e5e605096
Omolara5861 <debbiegterra@gmail.com>
2022-06-01 09:57:33 +0100

    npm superheroes lib

---
+++ npm/npm.js
@@ -0,0 +1,16 @@
+//Getting Superheroes Name 
+const superheroes = require('superheroes');
+
+//Generating all Superheroes Names
+const allSuperheroes = superheroes.all;
+
+//Generating random Superheroes Names
+const randomSuperheroes = superheroes.random();
+
+//Log all Superheroes Name to the console
+console.log(allSuperheroes);
+
+//Log random Superheroes Name to the console
+console.log(randomSuperheroes);
+
+
