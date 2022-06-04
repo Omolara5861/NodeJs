3d53cadae706491e56666523c0841dccdd21fe23
Omolara5861 <debbiegterra@gmail.com>
2022-06-04 18:09:48 +0100

    instlled my lib

--- index.js
+++ index.js
@@ -87,4 +87,7 @@ const server = http.createServer((req , res) => {
     })
 });
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
+
