cd779860c5c95308d2ccccbe720c5a47801c1d4d
Omolara5861 <debbiegterra@gmail.com>
2022-06-18 22:29:57 +0100

    express routing

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello Express');
-}); 
+});
+
+app.get('/services', (req, res) => {
+    res.send('Services that I Offer');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Me')
+})
 
 const PORT = process.env.PORT || 4300;
 
