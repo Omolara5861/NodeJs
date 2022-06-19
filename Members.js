44e541f1ea1e427791b47ef566c1c5912a6cd6e6
Omolara5861 <debbiegterra@gmail.com>
2022-06-19 14:06:30 +0100

    using JSONs in express

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        'id': 1,
+        'name': 'John Doe',
+        'email': 'john@gmail.com',
+        'status': 'active'
+    },
+    {
+        'id': 2,
+        'name': 'James Bond',
+        'email': 'james@gmail.com',
+        'status': 'in-active'
+    },
+    {
+        'id': 3,
+        'name': 'Sherlock Holmes',
+        'email': 'sherlock@gmail.com',
+        'status': 'active'
+    },
+    {
+        'id': 4,
+        'name': 'Lady Stack',
+        'email': 'stack@gmil.com',
+        'status': 'in-active'
+    },
+]
+
+module.exports = members;
--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express');
 
 const app = express();
 
+// This is not n ideal way of loading files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+//use() is the method used to include middlewares
+// Sets public s the static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Dealing with JSONs in express 
+const members = require('./Members');
+// Route to get the members
+app.get('/api/members', (req, res) => res.json(members));
+
 
 
 const PORT = process.env.PORT || 4300;
